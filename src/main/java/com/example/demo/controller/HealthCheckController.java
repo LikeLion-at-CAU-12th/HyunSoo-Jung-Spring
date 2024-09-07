987a0731011cf11789da0194f2834b05a38848f1
usernamehs <hsforwork123@gmail.com>
2024-09-08 01:13:17 +0900

    Delete

--- src/main/java/com/example/demo/controller/HealthCheckController.java
+++ src/main/java/com/example/demo/controller/HealthCheckController.java
@@ -1,22 +1,22 @@
-package com.example.demo.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@RestController
-@RequestMapping("/health")
-public class HealthCheckController {
-
-    @GetMapping("/v1")
-    public Map<String, String> healthCheck() {
-        // response
-        Map<String, String> response = new HashMap();
-        response.put("status", "OK");
-
-        return response;
-    }
-}
+//package com.example.demo.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@RestController
+//@RequestMapping("/health")
+//public class HealthCheckController {
+//
+//    @GetMapping("/v1")
+//    public Map<String, String> healthCheck() {
+//        // response
+//        Map<String, String> response = new HashMap();
+//        response.put("status", "OK");
+//
+//        return response;
+//    }
+//}
